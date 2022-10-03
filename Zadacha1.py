d21b8bf3172934dd17d89ebe8611d29a4bc94f06
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-10-03 12:27:00 +0300

    commit6

--- Zadacha1.py
+++ Zadacha1.py
@@ -5,3 +5,15 @@
 
 # 6782 -> 23
 # 0,56 -> 11
+
+
+print('Введите число ')
+n = input()
+summ = 0
+
+
+for i in str(n):
+    if i != '.':
+        summ += int(i)
+
+print(f'Сумма чисел равна: {summ}')
